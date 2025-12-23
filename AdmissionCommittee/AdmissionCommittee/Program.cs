ecb29394738a441e23ea646c0f5ebe1e892bd532
Yarik543 <162128013+Yarik543@users.noreply.github.com>
2025-12-23 19:22:05 +0300

    Logi (#5)
    * Добавил логи
    
    * Исправил ошибки

--- AdmissionCommittee/AdmissionCommittee/Program.cs
+++ AdmissionCommittee/AdmissionCommittee/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Windows.Forms;
 
 namespace AdmissionCommittee
@@ -8,6 +9,12 @@ namespace AdmissionCommittee
         [STAThread]
         static void Main()
         {
+            Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .WriteTo.File("logs/performance.log",
+            rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+
             ApplicationConfiguration.Initialize();
             System.Windows.Forms.Application.Run(new MainForm());
         }
