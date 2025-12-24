adfe530a6d9c2d01bd3d898e2509c78d1f3ab829
Yarik543 <aroslavkasymbekov@gmail.com>
2025-12-24 21:16:56 +0300

    Добавил mvc проект

---
+++ AdmissionCommittee/AdmissionCommittee.Web/Controllers/ApplicantsController.cs
@@ -0,0 +1,65 @@
+﻿using AdmissionCommittee.Abstractions.Services;
+using AdmissionCommittee.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdmissionCommittee.Web.Controllers;
+
+public class ApplicantsController : Controller
+{
+    private readonly IApplicantService _service;
+
+    public ApplicantsController(IApplicantService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var list = await _service.GetAllAsync();
+        return View(list);
+    }
+
+    public IActionResult Create()
+    {
+        return View(new Applicant());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Applicant applicant)
+    {
+        if (!ModelState.IsValid)
+            return View(applicant);
+
+        await _service.AddAsync(applicant);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        var applicant = (await _service.GetAllAsync())
+            .FirstOrDefault(a => a.Id == id);
+
+        if (applicant == null)
+            return NotFound();
+
+        return View(applicant);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Applicant applicant)
+    {
+        if (!ModelState.IsValid)
+            return View(applicant);
+
+        await _service.UpdateAsync(applicant);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _service.RemoveAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ AdmissionCommittee/AdmissionCommittee.Web/Program.cs
@@ -0,0 +1,27 @@
+﻿using AdmissionCommittee.Abstractions.Repositories;
+using AdmissionCommittee.Abstractions.Services;
+using AdmissionCommittee.Data.EF;
+using AdmissionCommittee.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AdmissionCommitteeDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IApplicantRepository, ApplicantEfRepository>();
+builder.Services.AddScoped<IApplicantService, ApplicantService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Applicants}/{action=Index}/{id?}");
+
+app.Run();
